00dd0c86411b06a31ac71b89a5a05a0cfee77250
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-03-22 17:46:03 +0000

    missing styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,189 +1,224 @@
 import React, { useState, useEffect } from "react";
 
 const AnyComponent = () => {
-    const [tasks, setTasks] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-    const fetchTodoList = () => {
-        console.log(' fetch todo list for user')
-        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${username}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-                }
-        }).then(response => {
-            if(response.ok) {
-                return response.json()
-            }
-            throw Error(response.status + "! Something went wrong")
-        }).then((todoData) => {
-            console.log(toDoData);
-        }).catch(err => {
-            console.log('Error', err)
-        })
-      
-    }
+  const [tasks, setTasks] = useState([]);
+  const [newTaskName, setNewTaskName] = useState("");
 
-    const addUser = () => {
-        if(inputValue.trim() !== '');
-        fetch(`'https://playground.4geeks.com/apis/fake/todos/user/${username}'`,{
-            method: 'POST',
-            mode: 'cors',
-            redirect: 'follow',
-            headers: new Headers({
-            'Content-Type': 'text/plain'
-            })
+  const getUserTodos = () => {
+    fetch (`https://playground.4geeks.com/apis/fake/todos/user/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }, 
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         }
-       
-        ).then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch todos');
-            }
-            return response.json();
-        })
-        .then(data => {
-            setTasks(data);
-            setInputValue('');
-        })
-        .catch(error => {
-            
-            setError('Failed to fetch todos');
-        });{
-            setTasks([...tasks, inputValue]);
-            
-        } 
-        setUsername([...username, inputValue])
-        setUsername('');
-    }
+        throw Error(response.status + "! Something went wrong");
+      })
+      .then((todoData) => {
+        console.log("todoData");
+        console.log(todoData);
+        setTasks(todoData);
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err);
+      });
+  };
 
-    const deleteUser = () => {
-        console.log('Delete username')
-    }
+  const addUser = async (event) => {
+    //prevenir o browser de fazer refresh dps de clickares no botao
+    event.preventDefault();
 
-    const addTask = () => {
-        if (inputValue.trim() !== '') 
-        fetch(`'https://playground.4geeks.com/apis/fake/todos/user/${username}'`,{
-            method: 'POST' ,
-            headers: {
-                "Content-Type": "application/json"
-              }
+    //if useername field is not empty the code runs
+    if (username.trim() !== "") {
+      //api call to create user/todo list
+      const response = await fetch(
+        `https://playground.4geeks.com/apis/fake/todos/user/${username}`,
+        {
+          method: "POST",
+          mode: "cors",
+          redirect: "follow",
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+          body: JSON.stringify([]),
         }
-       
-        ).then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch todos');
-            }
-            return response.json();
-        })
-        .then(data => {
-            setTasks(data);
-            setInputValue('');
-        })
-        .catch(error => {
-            
-            setError('Failed to fetch todos');
-        });{
-            setTasks([...tasks, inputValue]);
-            
-        } 
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch todos");
+      }
+
+      setTasks([]);
+    } else {
+      //if input is empty alert the user
+      alert("Please add a name to the input");
     }
+  };
 
-    const handleKeyEnter = (e) => {
-        if (e.key === 'Enter') {
-            addTask();
-        } 
-    };
-
-    const removeTask = (index) => {
-        fetch(`'https://playground.4geeks.com/apis/fake/todos/user/${username}'`,{
-            method: 'DELETE' ,
-            headers: {
-                "Content-Type": "application/json"
-              }
+  const deleteUser = () => {
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/${username}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch todos");
         }
-       
-        ).then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch todos');
+        return response.json();
+      })
+      .then((data) => {
+        //   setTasks(data);
+        setUsername("");
+        setTasks([]);
+      })
+      .catch((error) => {
+        setError("Failed to addTask");
+        console.log(error);
+      });
+  };
+
+  const addTask = () => {
+    const updatedTasks = [...tasks, { done: false, label: newTaskName }];
+
+    if (newTaskName.trim() !== "") {
+      fetch(`https://playground.4geeks.com/apis/fake/todos/user/${username}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTasks),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch todos");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          //   setTasks(data);
+          setNewTaskName("");
+        })
+        .then(() => {
+          getUserTodos();
+        })
+        .catch((error) => {
+          setError("Failed to addTask");
+          console.log(error);
+        });
+    } else {
+      alert("task needs a name");
+    }
+  };
+
+  const handleKeyEnter = (e) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
+
+  const updateTask = (id) => {
+    const updatedTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    console.log("updatedTasks");
+    console.log(updatedTasks);
+
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/${username}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTasks),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch todos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // setTasks(data);
+      })
+      .then(() => {
+        getUserTodos();
+      })
+      .catch((error) => {
+        setError("Failed to Update todos");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="container mt-3">
+      <h5 className="Title text-center mt-5 mb-2 p-3">Todos List</h5>
+      <div>
+        <input
+          className="inputUser"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter your Username"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              console.log(" Fetch Todo List");
+              fetchTodoList();
             }
-            return response.json();
-        })
-        .then(data => {
-            setTasks(data);
-            setInputValue('');
-        })
-        .catch(error => {
-            
-            setError('Failed to fetch todos');
-        });{
-            setTasks([...tasks, inputValue]);
-            
-        } 
-        const updatedTasks = [...tasks];
-        updatedTasks.splice(index, 1);
-        setTasks(updatedTasks);
-    };
+          }}
+        />
+        <button onClick={addUser}>Create User</button>
+        <button onClick={getUserTodos}>Get User</button>
+        <button onClick={deleteUser}>Delete User</button>
+      </div>
+      <nav className="navbar navbar-light bg-light">
+        <input
+          className="Tasker container"
+          type="text"
+          onChange={(e) => setNewTaskName(e.target.value)}
+          onKeyPress={handleKeyEnter}
+          placeholder="What needs to be done"
+          maxLength={80}
+          value={newTaskName}
+        />
+      </nav>
 
-    
+      <div className="TaskRemover mt-2">
+        {tasks?.length > 0 &&
+          tasks.map((task, index) => {
+            console.log("task");
+            console.log(task);
 
-    return (
-        <div className="container mt-3">
-            
-            <h5 className="Title text-center mt-5 mb-2 p-3">Todos List</h5>
-            <div>
-                <input
-                className="inputUser"
-                type="text"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                placeholder="Enter your Username"
-              
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter' ) {
-                        console.log(' Fetch Todo List')
-                        fetchTodoList()
-                    }
-                }}
-                />
-                <button onClick={deleteUser}>Delete User</button>
-            </div>
-            <nav className="navbar navbar-light bg-light">
-                <input
-                    className="Tasker container"
-                    type="text"
-                    onChange={e => setInputValue(e.target.value)}
-                    onKeyPress={handleKeyEnter}
-                    placeholder="What needs to be done"
-                    maxLength={80}
-                    value={inputValue}
-                />
-            </nav>
-            
-
-                <div className="TaskRemover mt-2">
-                    {tasks.map((task, index) => (
-                        <div className="ListTasks d-flex container p-0" key={index}>
-                            <div className="navbar navbar-light bg-light container p-4">
-                                <input type="checkbox"
-                                checked=""
-                                />
-                                <p className="Newtasks mb-0">{task}</p>
-                                <span className="spanIcone">
-                                    <i
-                                        className="fa-solid fa-ban "
-                                        onClick={() => removeTask(index)}
-                                    ></i>
-                                </span>
-                            </div>
-                        </div>
-                    ))}
+            return (
+              <div className="ListTasks d-flex container p-0" key={index}>
+                <div className="navbar navbar-light bg-light container p-4">
+                  {/* <input type="checkbox" checked={true} /> */}
+                  <p className="Newtasks mb-0">{task.label}</p>
+                  <span className="spanIcone">
+                    <i
+                      className="fa-solid fa-ban "
+                      onClick={() => updateTask(task.id)}
+                    ></i>
+                  </span>
                 </div>
-        
-            <div className="TasksNum bg-light mt-2 p-2">{tasks.length} Tasks left to do</div>
-           
-        </div>
-    );
+              </div>
+            );
+          })}
+      </div>
+
+      <div className="TasksNum bg-light mt-2 p-2">
+        {tasks.length} Tasks left to do
+      </div>
+    </div>
+  );
 };
 
 export default AnyComponent;
